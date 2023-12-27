bd0720057f0a2e259cdb8e29f348a88ec449740e
Davidvn95 <davidvergaraok@gmail.com>
2023-12-26 20:46:14 -0500

    agregando animaciones de scroll, links de los proyectos, botón del formulario

--- src/utils/projects-resource.js
+++ src/utils/projects-resource.js
@@ -2,32 +2,38 @@ export const data = [
   {
     name: 'Space Food Store',
     image: '/projects/space-food.png',
-    description: 'Aplicación creada para algo'
+    description: 'Aplicación creada para algo',
+    demo: 'https://spacefood.netlify.app/'
   },
   {
     name: 'Space Food Admin',
     image: '/projects/space-food-admin.png',
-    description: 'Aplicación creada para algo'
+    description: 'Aplicación creada para algo',
+    demo: 'https://storespacefood.netlify.app/'
   },
   {
     name: 'Good Reads',
     image: '/projects/good-reads.png',
-    description: 'Aplicación creada para algo'
+    description: 'Aplicación creada para algo',
+    demo: 'https://goodreads-midu.vercel.app/'
   },
   {
     name: 'Memory Game',
     image: '/projects/memory-game.png',
-    description: 'Aplicación creada para algo'
+    description: 'Aplicación creada para algo',
+    demo: 'https://memory-game-pink-two.vercel.app/'
   },
   {
     name: 'Pokedex',
     image: '/projects/pokedex.png',
-    description: 'Aplicación creada para algo'
+    description: 'Aplicación creada para algo',
+    demo: 'https://front-pokemons.vercel.app/'
   },
   {
     name: 'Rick & Morty',
     image: '/projects/rick&morty.png',
-    description: 'Aplicación creada para algo'
+    description: 'Aplicación creada para algo',
+    demo: 'https://rick-and-morty-app-sigma-beige.vercel.app/'
   }
 ]
 
@@ -56,5 +62,5 @@ export const links = [
     link: 'contact',
     name: 'Contáctame',
     icon: 'contact.png'
-  },
+  }
 ]
