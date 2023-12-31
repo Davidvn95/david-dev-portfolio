d4648d4b92bb6fc577b8a1008b020b3fe7673392
Davidvn95 <davidvergaraok@gmail.com>
2023-12-30 21:49:54 -0500

    Mejorando cards projects

--- src/utils/projects-resource.js
+++ src/utils/projects-resource.js
@@ -2,38 +2,50 @@ export const data = [
   {
     name: 'Space Food Store',
     image: '/projects/space-food.png',
-    description: 'Aplicación creada para algo',
-    demo: 'https://spacefood.netlify.app/'
+    description:
+      'Aplicación de pedidos con integracion de pasarela de págos, autenticación con Google y Facebook y actualización de estados de los pedidos mediante socket.io',
+    demo: 'https://spacefood.netlify.app/',
+    code: 'https://github.com/EXPRESSFOOD2/customerclient'
   },
   {
     name: 'Space Food Admin',
     image: '/projects/space-food-admin.png',
-    description: 'Aplicación creada para algo',
-    demo: 'https://storespacefood.netlify.app/'
+    description:
+      'Aplicación administrativa con autenticación, encargada de recibir los pedidos y darles salida, así como actualizar el estado del mismo; control de invintario y de usuarios registrados en la aplicación de compras.',
+    demo: 'https://storespacefood.netlify.app/',
+    code: 'https://github.com/EXPRESSFOOD2/storeclient'
   },
   {
     name: 'Good Reads',
     image: '/projects/good-reads.png',
-    description: 'Aplicación creada para algo',
-    demo: 'https://goodreads-midu.vercel.app/'
+    description:
+      'Aplicación creada como participación a los retos de programación del creador de contenido, midudev, cuenta con filtros combinados, sincronización de pestañas y persistencia de datos',
+    demo: 'https://goodreads-midu.vercel.app/',
+    code: 'https://github.com/Davidvn95/pruebas-tecnicas-Midudev/tree/feature/davidvn95-reading-list/pruebas/01-reading-list/Davidvn95/reading-lest-davidvn95'
   },
   {
     name: 'Memory Game',
     image: '/projects/memory-game.png',
-    description: 'Aplicación creada para algo',
-    demo: 'https://memory-game-pink-two.vercel.app/'
+    description:
+      'Aplicación de juego de memoria didáctico pensada para niños, para desarrollar su memoria y atención, es una PWA para que pueda ser instalada en los dispositivos desde el navegador.',
+    demo: 'https://memory-game-pink-two.vercel.app/',
+    code: 'https://github.com/Davidvn95/Memory_Game'
   },
   {
     name: 'Pokedex',
     image: '/projects/pokedex.png',
-    description: 'Aplicación creada para algo',
-    demo: 'https://front-pokemons.vercel.app/'
+    description:
+      'Aplicación de pokemos creada para colocar en práctica mis conocimientos adquiridos sobre las herramientas del ecosistema PERN.',
+    demo: 'https://front-pokemons.vercel.app/',
+    code: 'https://github.com/Davidvn95/Front-Pokemons'
   },
   {
     name: 'Rick & Morty',
     image: '/projects/rick&morty.png',
-    description: 'Aplicación creada para algo',
-    demo: 'https://rick-and-morty-app-sigma-beige.vercel.app/'
+    description:
+      'Aplicación de react con la que di mis primeros pasos en la programación web.',
+    demo: 'https://rick-and-morty-app-sigma-beige.vercel.app/',
+    code: 'https://github.com/Davidvn95/Rick-And-Morty-APP'
   }
 ]
 
