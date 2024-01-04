1e2635dbb157ff4d5582d7f041653b85664d8605
Davidvn95 <davidvergaraok@gmail.com>
2024-01-04 17:47:46 -0500

    colocando el envio de emails en el servidor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import vercel from "@astrojs/vercel/serverless";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-})
+  integrations: [tailwind(), preact()],
+  output: 'hybrid',
+  adapter: vercel()
+});
---
+++ src/pages/api/send-email.ts
@@ -0,0 +1,46 @@
+import type { APIRoute } from 'astro'
+import { SMTPClient } from 'emailjs'
+
+export const POST: APIRoute = async ({ request }) => {  
+  const data = await request.formData()
+  const name = data.get('name')
+  const emailAddress = data.get('email')
+  const subject = data.get('subject')
+  const message = data.get('message')
+
+  const client = new SMTPClient({
+    host: 'smtp.gmail.com',
+    port: 465,
+    user: import.meta.env.GOOGLE_ACCOUNT,
+    password: import.meta.env.GOOGLE_PASSWORD,
+    ssl: true
+  })
+
+  const response = await client.sendAsync({
+    text: `<h1>Mensaje de: ${name}</h1>
+      <h3>Emisor: ${emailAddress}</h3>
+      <p>${message}</p>`,
+    to: 'david.vergara.dev@gmail.com',
+    from: 'david.vergara.dev@gmail.com',
+    subject: JSON.stringify(subject),
+    attachment: [
+      {
+        data: `
+      <html>
+        <h1>Mensaje de: ${name}</h1>
+        <h3>Emisor: ${emailAddress}</h3>
+        <p>${message}</p>
+      </html>`,
+        alternative: true
+      }
+    ]
+  })
+
+  if (response.checkValidity().isValid) {
+    return new Response(JSON.stringify({ success: true }), {
+      status: 200
+    })
+  } else {
+    return new Response(JSON.stringify({ success: false }))
+  }
+}
