c1cf79fab4fd9920cd24ac2f1f22874d52f52d7f
David Vergara <davidvergaraok@gmail.com>
2024-03-19 16:35:29 -0500

    Cambiando CV, agregando proyecto y actualizando experiencia

--- src/libs/data.js
+++ src/libs/data.js
@@ -1,11 +1,19 @@
 export const data = [
   {
+    name: 'Personal Chef',
+    image: '/projects/personal-chef.webp',
+    description:
+      'Esta app fué creada como prueba técnica con la cual entré a ser parte del equipo de desarrollo frontend en Gravitad, está desarrollada con Qwik y TypeScript',
+    demo: 'https://personal-chef-five.vercel.app/',
+    code: 'https://github.com/Davidvn95/personal-chef',
+  },
+  {
     name: 'Space Food Store',
     image: '/projects/space-food.avif',
     description:
       'Aplicación de pedidos con integracion de pasarela de págos, autenticación con Google y Facebook y actualización de estados de los pedidos mediante socket.io',
     demo: 'https://spacefood.netlify.app/',
-    code: 'https://github.com/EXPRESSFOOD2/customerclient'
+    code: 'https://github.com/EXPRESSFOOD2/customerclient',
   },
   {
     name: 'Space Food Admin',
@@ -13,7 +21,7 @@ export const data = [
     description:
       'Aplicación administrativa con autenticación, encargada de recibir los pedidos y darles salida, así como actualizar el estado del mismo; control de invintario y de usuarios registrados en la aplicación de compras.',
     demo: 'https://storespacefood.netlify.app/',
-    code: 'https://github.com/EXPRESSFOOD2/storeclient'
+    code: 'https://github.com/EXPRESSFOOD2/storeclient',
   },
   {
     name: 'Good Reads',
@@ -21,7 +29,7 @@ export const data = [
     description:
       'Aplicación creada como participación a los retos de programación del creador de contenido, midudev, cuenta con filtros combinados, sincronización de pestañas y persistencia de datos',
     demo: 'https://goodreads-midu.vercel.app/',
-    code: 'https://github.com/Davidvn95/pruebas-tecnicas-Midudev/tree/feature/davidvn95-reading-list/pruebas/01-reading-list/Davidvn95/reading-lest-davidvn95'
+    code: 'https://github.com/Davidvn95/pruebas-tecnicas-Midudev/tree/feature/davidvn95-reading-list/pruebas/01-reading-list/Davidvn95/reading-lest-davidvn95',
   },
   {
     name: 'Memory Game',
@@ -29,7 +37,7 @@ export const data = [
     description:
       'Aplicación de juego de memoria didáctico pensada para niños, para desarrollar su memoria y atención, es una PWA para que pueda ser instalada en los dispositivos desde el navegador.',
     demo: 'https://memory-game-pink-two.vercel.app/',
-    code: 'https://github.com/Davidvn95/Memory_Game'
+    code: 'https://github.com/Davidvn95/Memory_Game',
   },
   {
     name: 'Pokedex',
@@ -37,42 +45,41 @@ export const data = [
     description:
       'Aplicación de pokemos creada para colocar en práctica mis conocimientos adquiridos sobre las herramientas del ecosistema PERN.',
     demo: 'https://front-pokemons.vercel.app/',
-    code: 'https://github.com/Davidvn95/Front-Pokemons'
+    code: 'https://github.com/Davidvn95/Front-Pokemons',
   },
   {
     name: 'Rick & Morty',
     image: '/projects/rick&morty.webp',
-    description:
-      'Aplicación de react con la que di mis primeros pasos en la programación web.',
+    description: 'Aplicación de react con la que di mis primeros pasos en la programación web.',
     demo: 'https://rick-and-morty-app-sigma-beige.vercel.app/',
-    code: 'https://github.com/Davidvn95/Rick-And-Morty-APP'
-  }
+    code: 'https://github.com/Davidvn95/Rick-And-Morty-APP',
+  },
 ]
 
 export const links = [
   {
     link: 'home',
     name: 'Inicio',
-    icon: 'home.svg'
+    icon: 'home.svg',
   },
   {
     link: 'about',
     name: 'Sobre mí',
-    icon: 'about.svg'
+    icon: 'about.svg',
   },
   {
     link: 'experience',
     name: 'Experiencia',
-    icon: 'experience.svg'
+    icon: 'experience.svg',
   },
   {
     link: 'my-job',
     name: '¿Qué hago?',
-    icon: 'my-job.svg'
+    icon: 'my-job.svg',
   },
   {
     link: 'contact',
     name: 'Contáctame',
-    icon: 'contact.svg'
-  }
+    icon: 'contact.svg',
+  },
 ]
